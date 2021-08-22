b917f520c16a28232381b3a6a965520f15dfdd4d
Iтѕ_Ѵιcнч#1337 <66045929+Its-Vichy@users.noreply.github.com>
2021-08-22 10:16:29 +0200

    🐌 Installation script
    - Create requirements.txt
    - Create install.sh
    > Still in developpement !

---
+++ script/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
+clear && echo -e "\033[2J\n _    _ ____        _\n| |  | |  _ \      | |\n| |__| | |_) | ___ | |_\n|  __  |  _ < / _ \| __|\n| |  | | |_) | (_) | |_\n|_|  |_|____/ \___/ \__|\n"
+
+echo Updating system.
+sudo apt-get update -y && sudo apt-get upgrade -y
+
+echo Installing packages.
+sudo apt-get install -y screen python3 python3-pip
+
+echo Installation of dependencies.
+pip3 install -r ./requirements.txt
+
+echo Installation finished.
